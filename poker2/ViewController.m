9de406e7e1439b98ae162997e1a85b6dbc35272c
作田 諒@SYS <sakuda-ryo@dmm.com>
2015-02-05 17:20:09 +0900

    start

--- poker2/ViewController.m
+++ poker2/ViewController.m
@@ -8,20 +8,80 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () 
+@property (strong, nonatomic) IBOutlet UICollectionView *myCollectionView;
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    
+    NSMutableArray *moviePhotos;
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
+    [self.myCollectionView setDataSource:self];
+    [self.myCollectionView setDelegate:self];
+    
+    [self setData];
+    
+    
+    
+}- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void)setData{
+    
+    moviePhotos = [[NSMutableArray alloc]init];
+    
+    for (int i = 1; i <= 3; i++) {
+        NSString *filename = [NSString stringWithFormat:@"movie%d.jpg", i];
+        [moviePhotos addObject:[UIImage imageNamed:filename]];
+
+    }
+}
+
+#pragma mark -collection view delegate
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    //とりあえずセクションは2つ
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+
+    return moviePhotos.count;
+}
+
+
+//Method to create cell at index path
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UICollectionViewCell *cell;
+    
+    cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell1" forIndexPath:indexPath];
+//    cell.backgroundColor = [UIColor whiteColor];
+    
+    UIImageView *image = (UIImageView *)[cell viewWithTag:1];
+    image.backgroundColor = [UIColor redColor];
+    
+    //画像入れてる
+    [image setImage:moviePhotos[indexPath.row]];
+    
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    //クリックされたらよばれる
+    NSLog(@"Clicked %d-%d",indexPath.section,indexPath.row);
+}
+
 
 @end
